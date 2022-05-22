8a6fa38dee312d102bb6d374981a63929af35269
Роман Лодяты Александрович <68326671+roma111ka@users.noreply.github.com>
2022-05-22 13:26:30 +0300

    feat: 04 task done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 
+const directory = path.join(__dirname, 'files');
+const directoryCopy = path.join(__dirname, 'copyDirectory/');
+
+fs.rmdir(directoryCopy, () => {});
+fs.mkdir(directoryCopy, () => {});
+fs.readdir(directory, (error, data) => {
+  if (error) return console.error(error.message);
+
+  data.forEach(v => {
+      
+    let fileDirectory = path.join(directory, v);
+    let fileCopyDir = path.join(directoryCopy, v);
+    fs.copyFile(fileDirectory, fileCopyDir, () => {});
+
+  });
+});
